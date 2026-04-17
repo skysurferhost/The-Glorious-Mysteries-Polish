3da9944e00afb45cc52cc6ddf0a8592b16913890
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-04-16 22:57:11 -0400

    upload

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-c-cha-gisu-sng-li",
+      "name": "1. Zmartwychwstanie Pana Jezusa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20950249137904464,
+          "pitch": -0.4250589715753428,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-ln-tri"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.16823973861483843,
+          "pitch": -0.6735388679733862,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Zmartwychwstanie Pana Jezusa</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Po upływie szabatu, o świcie pierwszego dnia tygodnia przyszła Maria Magdalena i druga Maria obejrzeć grób. A oto powstało wielkie trzęsienie ziemi. Albowiem anioł Pański zstąpił z nieba, podszedł, odsunął kamień i usiadł na nim. Postać jego jaśniała jak błyskawica, a szaty jego były białe jak śnieg. Ze strachu przed nim zadrżeli strażnicy i stali się jakby umarli. Anioł zaś przemówił do niewiast: «Wy się nie bójcie! Gdyż wiem, że szukacie Jezusa Ukrzyżowanego. Nie ma Go tu, bo zmartwychwstał, jak powiedział. Chodźcie, zobaczcie miejsce, gdzie leżał. A idźcie szybko i powiedzcie Jego uczniom: Powstał z martwych i oto udaje się przed wami do Galilei. Tam Go ujrzycie. Oto, co wam powiedziałem». Pośpiesznie więc oddaliły się od grobu, z bojaźnią i wielką radością, i biegły oznajmić to Jego uczniom.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">A oto Jezus stanął przed nimi i rzekł: «Witajcie!» One podeszły do Niego, objęły Go za nogi i oddały Mu pokłon. A Jezus rzekł do nich: «Nie bójcie się! Idźcie i oznajmijcie moim braciom: niech idą do Galilei, tam Mnie zobaczą».<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Mateusz 28:1–10)</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-c-cha-gisu-ln-tri",
+      "name": "2. Wniebowstąpienie Pana Jezusa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8295190152248875,
+          "pitch": -0.16886677939099215,
+          "rotation": 0,
+          "target": "2-3-c-cha-thnh-thn-hin-xung"
+        },
+        {
+          "yaw": -2.3393628150142955,
+          "pitch": 0.12362877656975968,
+          "rotation": 0,
+          "target": "0-1-c-cha-gisu-sng-li"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7809236815983347,
+          "pitch": -0.5434597421898903,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Wniebowstąpienie Pana Jezusa</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Pierwszą Księgę napisałem, Teofilu, o wszystkim, co Jezus czynił i czego nauczał od początku aż do dnia, w którym udzielił przez Ducha Świętego poleceń Apostołom, których sobie wybrał, a potem został wzięty do nieba. Im też po swojej męce dał wiele dowodów, że żyje: ukazywał się im przez czterdzieści dni i mówił o królestwie Bożym. A podczas wspólnego posiłku kazał im nie odchodzić z Jerozolimy, ale oczekiwać obietnicy Ojca: «Słyszeliście o niej ode Mnie - [mówił] - Jan chrzcił wodą, ale wy wkrótce zostaniecie ochrzczeni Duchem Świętym».</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Zapytywali Go zebrani: «Panie, czy w tym czasie przywrócisz królestwo Izraela?» Odpowiedział im: «Nie wasza to rzecz znać czasy i chwile, które Ojciec ustalił swoją władzą, ale gdy Duch Święty zstąpi na was, otrzymacie Jego moc i będziecie moimi świadkami w Jerozolimie i w całej Judei, i w Samarii, i aż po krańce ziemi».</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Po tych słowach uniósł się w ich obecności w górę i obłok zabrał Go im sprzed oczu. Kiedy uporczywie wpatrywali się w Niego, jak wstępował do nieba, przystąpili do nich dwaj mężowie w białych szatach. I rzekli: «Mężowie z Galilei, dlaczego stoicie i wpatrujecie się w niebo? Ten Jezus, wzięty od was do nieba, przyjdzie tak samo, jak widzieliście Go wstępującego do nieba».<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Dzieje Apostolskie 1:1–11)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-c-cha-thnh-thn-hin-xung",
+      "name": "3. Zesłanie Ducha Świętego",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07506402957144687,
+          "pitch": -0.10977309799296009,
+          "rotation": 0,
+          "target": "3-4-c-cha-tri-cho-c-b-ln-tri"
+        },
+        {
+          "yaw": 1.8835112084244283,
+          "pitch": -0.03659732029855434,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-ln-tri"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.11496180811354506,
+          "pitch": -0.37224441421833276,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Zesłanie Ducha Świętego</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Kiedy nadszedł wreszcie dzień Pięćdziesiątnicy, znajdowali się wszyscy razem na tym samym miejscu. Nagle dał się słyszeć z nieba szum, jakby uderzenie gwałtownego wiatru, i napełnił cały dom, w którym przebywali. Ukazały się im też języki jakby z ognia, które się rozdzieliły, i na każdym z nich spoczął jeden. I wszyscy zostali napełnieni Duchem Świętym, i zaczęli mówić obcymi językami, tak jak im Duch pozwalał mówić.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Przebywali wtedy w Jerozolimie pobożni Żydzi ze wszystkich narodów pod słońcem. Kiedy więc powstał ów szum, zbiegli się tłumnie i zdumieli, bo każdy słyszał, jak przemawiali w jego własnym języku. «Czyż ci wszyscy, którzy przemawiają, nie są Galilejczykami?» - mówili pełni zdumienia i podziwu. «Jakżeż więc każdy z nas słyszy swój własny język ojczysty? - Partowie i Medowie, i Elamici, i mieszkańcy Mezopotamii, Judei oraz Kapadocji, Pontu i Azji, Frygii oraz Pamfilii, Egiptu i tych części Libii, które leżą blisko Cyreny, i przybysze z Rzymu, Żydzi oraz prozelici, Kreteńczycy i Arabowie - słyszymy ich głoszących w naszych językach wielkie dzieła Boże». Zdumiewali się wszyscy i nie wiedzieli, co myśleć: «Co ma znaczyć?» - mówili jeden do drugiego. «Upili się młodym winem» - drwili inni.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Dzieje Apostolskie 2:1–13)</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-c-cha-tri-cho-c-b-ln-tri",
+      "name": "4. Wniebowzięcie Najświętszej Maryi Panny",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.28290339959050925,
+          "pitch": -0.3424279478755068,
+          "rotation": 0,
+          "target": "4-5-c-cha-tri-thng-c-m-trn-tri"
+        },
+        {
+          "yaw": 2.3791351372577685,
+          "pitch": 0.184547861773261,
+          "rotation": 0,
+          "target": "2-3-c-cha-thnh-thn-hin-xung"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2414294338391887,
+          "pitch": -0.5338737219185532,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Wniebowzięcie Najświętszej Maryi Panny</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Wtedy Maryja rzekła: «Wielbi dusza moja Pana, i raduje się duch mój w Bogu, moim Zbawcy.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Bo wejrzał na uniżenie Służebnicy swojej. Oto bowiem błogosławić mnie będą odtąd wszystkie pokolenia,</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">gdyż wielkie rzeczy uczynił mi Wszechmocny. Święte jest Jego imię -</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">a swoje miłosierdzie na pokolenia i pokolenia [zachowuje] dla tych, co się Go boją.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">On przejawia moc ramienia swego, rozprasza [ludzi] pyszniących się zamysłami serc swoich.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Strąca władców z tronu, a wywyższa pokornych.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Głodnych nasyca dobrami, a bogatych z niczym odprawia.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Ujął się za sługą swoim, Izraelem, pomny na miłosierdzie swoje - jak przyobiecał naszym ojcom - na rzecz Abrahama i jego potomstwa na wieki».<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Łukasz 1:46–55)</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-c-cha-tri-thng-c-m-trn-tri",
+      "name": "5. Ukoronowanie Maryi na Królową Nieba i Ziemi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.749964359811174,
+          "pitch": 0.045768055644552774,
+          "rotation": 0,
+          "target": "3-4-c-cha-tri-cho-c-b-ln-tri"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6304062715925411,
+          "pitch": -0.20865712269064218,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Ukoronowanie Maryi na Królową Nieba i Ziemi</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Potem wielki znak się ukazał na niebie: Niewiasta obleczona w słońce i księżyc pod jej stopami, a na jej głowie wieniec z gwiazd dwunastu. A jest brzemienna. I woła cierpiąc bóle i męki rodzenia. I inny znak się ukazał na niebie: Oto wielki Smok barwy ognia, mający siedem głów i dziesięć rogów - a na głowach jego siedem diademów. I ogon jego zmiata trzecią część gwiazd nieba: i rzucił je na ziemię. I stanął Smok przed mającą rodzić Niewiastą, ażeby skoro porodzi, pożreć jej dziecię. I porodziła Syna - Mężczyznę, który wszystkie narody będzie pasł rózgą żelazną. I zostało porwane jej Dziecię do Bogai do Jego tronu. A Niewiasta zbiegła na pustynię, gdzie miejsce ma przygotowane przez Boga, aby ją tam żywiono przez tysiąc dwieście sześćdziesiąt dni.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">I nastąpiła walka na niebie: Michał i jego aniołowie mieli walczyć ze Smokiem. I wystąpił do walki Smok i jego aniołowie, ale nie przemógł, i już się miejsce dla nich w niebie nie znalazło. I został strącony wielki Smok, Wąż starodawny, który się zwie diabeł i szatan, zwodzący całą zamieszkałą ziemię, został strącony na ziemię, a z nim strąceni zostali jego aniołowie. I usłyszałem donośny głos mówiący w niebie:</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">«Teraz nastało zbawienie, potęga i królowanie Boga naszego i władza Jego Pomazańca, bo oskarżyciel braci naszych został strącony, ten, co dniem i nocą oskarża ich przed Bogiem naszym. A oni zwyciężyli dzięki krwi Baranka i dzięki słowu swojego świadectwa i nie umiłowali dusz swych - aż do śmierci. Dlatego radujcie się, niebiosa i ich mieszkańcy! Biada ziemi i biada morzu - bo zstąpił do was diabeł, pałając wielkim gniewem, świadom, że mało ma czasu».</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">A kiedy ujrzał Smok, że został strącony na ziemię, począł ścigać Niewiastę, która porodziła Mężczyznę. I dano Niewieście dwa skrzydła orła wielkiego, by na pustynię leciała na swoje miejsce, gdzie jest żywiona przez czas i czasy, i połowę czasu, z dala od Węża. A Wąż za Niewiastą wypuścił z gardzieli wodę jak rzekę, żeby ją rzeka uniosła. Lecz ziemia przyszła z pomocą Niewieście i otworzyła ziemia swą gardziel, i pochłonęła rzekę, którą Smok ze swej gardzieli wypuścił. I rozgniewał się Smok na Niewiastę, i odszedł rozpocząć walkę z resztą jej potomstwa, z tymi, co strzegą przykazań Boga i mają świadectwo Jezusa. I stanął na piasku [nad brzegiem] morza.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Apokalipsa 12:1–18)</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
